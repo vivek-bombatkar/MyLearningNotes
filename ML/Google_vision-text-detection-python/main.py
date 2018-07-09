8a67eeeac1b0f2224eda0e0f2d84996aab14d35d
GFK\vkbomb <vivek.bombatkar@gfk.com>
2018-07-09 19:18:04 +0200

    good

---
+++ ML/Google_vision-text-detection-python/main.py
@@ -0,0 +1,24 @@
+import io
+from google.cloud import vision
+
+
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
