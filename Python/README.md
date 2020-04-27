6ef7582a85ae0d3cb4b4c0f0b9f1408c443c6017
vivek <bombatkar.vivek@gmail.com>
2020-04-27 07:21:21 +0200

    keep learning...

--- Python/README.md
+++ Python/README.md
@@ -1083,7 +1083,14 @@ def handle(*, country: int,
 
 ## DESIGN PATTERNS in PYTHON
 > https://refactoring.guru/design-patterns/python
-- video guides
+- chcek video guides
+
+- Creational Patterns
+
+|Abstract Factory|Builder|Factory Method|Prototype|Singleton|  
+| --- | --- | --- | --- | --- |
+| Lets you produce families of related objects without specifying their concrete classes. |  Lets you construct complex objects step by step. The pattern allows you to produce different types and representations of an object using the same construction code.| Provides an interface for creating objects in a superclass, but allows subclasses to alter the type of objects that will be created. | Lets you copy existing objects without making your code dependent on their classes. | Lets you ensure that a class has only one instance, while providing a global access point to this instance. | 
+
 
 ## DBB - Behavior Driven Development
 > https://behave.readthedocs.io/en/latest/philosophy.html
---
+++ Python/design-patterns/all_in_one.py
@@ -0,0 +1,264 @@
+
+
+# Abstract Factory is a creational design pattern, which solves the problem of creating entire product families without specifying their concrete classes.
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+class AbstractFactory(ABC):
+
+    @abstractmethod
+    def create_product_a(self) -> AbstractProductA:
+        pass
+
+    @abstractmethod
+    def create_product_b(self) -> AbstractProductB:
+        pass
+
+class ConcreteFactory1(AbstractFactory):
+
+    def create_product_a(self) -> ConcreteProductA1:
+        return ConcreteProductA1()
+
+    def create_product_b(self) -> ConcreteProductB1:
+        return ConcreteProductB1()
+
+class ConcreteFactory2(AbstractFactory):
+
+    def create_product_a(self) -> ConcreteProductA2:
+        return ConcreteProductA2()
+
+    def create_product_b(self) -> ConcreteProductB2:
+        return ConcreteProductB2()
+
+class AbstractProductA(ABC):
+
+    @abstractmethod
+    def useful_function_a(self) -> str:
+        pass
+
+"""
+Concrete Products are created by corresponding Concrete Factories.
+"""
+
+
+class ConcreteProductA1(AbstractProductA):
+    def useful_function_a(self) -> str:
+        return "The result of the product A1."
+
+
+class ConcreteProductA2(AbstractProductA):
+    def useful_function_a(self) -> str:
+        return "The result of the product A2."
+
+class AbstractProductB(ABC):
+
+    @abstractmethod
+    def useful_function_b(self) -> None:
+        """
+        Product B is able to do its own thing...
+        """
+        pass
+
+    @abstractmethod
+    def another_useful_function_b(self, collaborator: AbstractProductA) -> None:
+
+        pass
+
+
+"""
+Concrete Products are created by corresponding Concrete Factories.
+"""
+
+
+class ConcreteProductB1(AbstractProductB):
+    def useful_function_b(self) -> str:
+        return "The result of the product B1."
+
+    def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
+        result = collaborator.useful_function_a()
+        return f"The result of the B1 collaborating with the ({result})"
+
+
+class ConcreteProductB2(AbstractProductB):
+    def useful_function_b(self) -> str:
+        return "The result of the product B2."
+
+    def another_useful_function_b(self, collaborator: AbstractProductA):
+
+        result = collaborator.useful_function_a()
+        return f"The result of the B2 collaborating with the ({result})"
+
+
+def client_code(factory: AbstractFactory) -> None:
+
+    product_a = factory.create_product_a()
+    product_b = factory.create_product_b()
+
+    print(f"{product_b.useful_function_b()}")
+    print(f"{product_b.another_useful_function_b(product_a)}", end="")
+
+
+# if __name__ == "__main__":
+#     """
+#     The client code can work with any concrete factory class.
+#     """
+#     print("Client: Testing client code with the first factory type:")
+#     client_code(ConcreteFactory1())
+#
+#     print("\n")
+#
+#     print("Client: Testing the same client code with the second factory type:")
+#     client_code(ConcreteFactory2())
+
+# Builder is a creational design pattern, which allows constructing complex objects step by step.
+
+from abc import ABC, abstractmethod, abstractproperty
+from typing import Any
+
+class Builder(ABC):
+    @abstractproperty
+    def product(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_a(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_b(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_c(self) -> None:
+        pass
+
+class ConcreteBuilder1(Builder):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._product = Product1()
+
+    @property
+    def product(self) -> Product1:
+        product = self._product
+        self.reset()
+        return product
+
+    def produce_part_a(self) -> None:
+        self._product.add("PartA1")
+
+    def produce_part_b(self) -> None:
+        self._product.add("PartB1")
+
+    def produce_part_c(self) -> None:
+        self._product.add("PartC1")
+
+class Product1():
+    def __init__(self) -> None:
+        self.parts = []
+
+    def add(self, part: Any) -> None:
+        self.parts.append(part)
+
+    def list_parts(self) -> None:
+        print(f"Product parts: {', '.join(self.parts)}", end="")
+
+class Director:
+    def __init__(self) -> None:
+        self._builder = None
+
+    @property
+    def builder(self) -> Builder:
+        return self._builder
+
+    @builder.setter
+    def builder(self, builder: Builder) -> None:
+        self._builder = builder
+
+    def build_minimal_viable_product(self) -> None:
+        self.builder.produce_part_a()
+
+    def build_full_featured_product(self) -> None:
+        self.builder.produce_part_a()
+        self.builder.produce_part_b()
+        self.builder.produce_part_c()
+#
+# if __name__ == "__main__":
+#     director = Director()
+#     builder = ConcreteBuilder1()
+#     director.builder = builder
+#
+#     print("Standard basic product: ")
+#     director.build_minimal_viable_product()
+#     builder.product.list_parts()
+#
+#     print("\n")
+#
+#     print("Standard full featured product: ")
+#     director.build_full_featured_product()
+#     builder.product.list_parts()
+#
+#     print("\n")
+#
+#     # Remember, the Builder pattern can be used without a Director class.
+#     print("Custom product: ")
+#     builder.produce_part_a()
+#     builder.produce_part_b()
+#     builder.product.list_parts()
+
+# Factory method is a creational design pattern which solves the problem of creating product objects without specifying their concrete classes.
+
+from abc import ABC, abstractmethod
+class Creator(ABC):
+    @abstractmethod
+    def factory_method(self):
+        pass
+
+    def some_operation(self) -> str:
+        # Call the factory method to create a Product object.
+        product = self.factory_method()
+
+        # Now, use the product.
+        result = f"Creator: The same creator's code has just worked with {product.operation()}"
+
+        return result
+
+class ConcreteCreator1(Creator):
+    def factory_method(self) -> ConcreteProduct1:
+        return ConcreteProduct1()
+
+
+class ConcreteCreator2(Creator):
+    def factory_method(self) -> ConcreteProduct2:
+        return ConcreteProduct2()
+
+class Product(ABC):
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+
+class ConcreteProduct1(Product):
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct1}"
+
+
+class ConcreteProduct2(Product):
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct2}"
+
+
+def client_code(creator: Creator) -> None:
+    print(f"Client: I'm not aware of the creator's class, but it still works.\n"
+          f"{creator.some_operation()}", end="")
+
+
+if __name__ == "__main__":
+    print("App: Launched with the ConcreteCreator1.")
+    client_code(ConcreteCreator1())
+    print("\n")
+
+    print("App: Launched with the ConcreteCreator2.")
+    client_code(ConcreteCreator2())
